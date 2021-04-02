715f6ecbc6872a074748ca59439caabb5bd15c69
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2021-04-02 21:07:58 +0100

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -39,7 +39,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <a>
                 <img
-                  src="/images/profile.jpg"
+                  src="https://avatars.githubusercontent.com/u/29145479"
                   className={`${styles.headerImage} ${utilStyles.borderCircle}`}
                   alt={name}
                 />
