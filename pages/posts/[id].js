a20c1b09a045d7e4a3f19f0193b47d110a788add
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2021-04-02 21:16:48 +0100

    Update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -8,7 +8,7 @@ export default function Post({ postData }) {
   return (
     <Layout>
       <Head>
-        <title>{postData.title}</title>
+        <title>{postData.title} - Blog | JacobHQ</title>
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
