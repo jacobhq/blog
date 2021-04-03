dbc2b243215b1e9742ec5a9022f301cc0a934dfc
Jacob Marshall <jacob.marshall.jem@gmail.com>
2021-04-03 21:37:16 +0100

    ✨ Commenting!

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,28 @@ import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
+import React, {Component} from "react";
+
+export class Comments extends Component {
+
+  componentDidMount () {
+      let script = document.createElement("script");
+      let anchor = document.getElementById("inject-comments-for-uterances");
+      script.setAttribute("src", "https://utteranc.es/client.js");
+      script.setAttribute("crossorigin","anonymous");
+      script.setAttribute("async", true);
+      script.setAttribute("repo", "jacobhq/blog");
+      script.setAttribute("issue-term", "pathname");
+      script.setAttribute( "theme", "photon-dark");
+      anchor.appendChild(script);
+  }
+
+  render() {
+    return (
+        <div id="inject-comments-for-uterances"></div>
+    );
+  }
+}
 
 export default function Post({ postData }) {
   return (
@@ -16,6 +38,7 @@ export default function Post({ postData }) {
           <Date dateString={postData.date} />
         </div>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} ></div>
+        <Comments />
       </article>
     </Layout>
   )
