686a88c3678f032b7f89ab33c4aec90357578963
Jacob Marshall <jacob.marshall.jem@gmail.com>
2021-04-03 21:15:09 +0100

    ✨ Syntax highlighting

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -15,7 +15,7 @@ export default function Post({ postData }) {
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} ></div>
       </article>
     </Layout>
   )
