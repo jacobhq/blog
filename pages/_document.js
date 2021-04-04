5c51c8384da4c83e8364380d6e22cdeb4b7ee96d
Jacob Marshall <jacob.marshall.jem@gmail.com>
2021-04-04 09:43:26 +0100

    🚑 Fix syntax highlighting

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="stylesheet" href="/prism.css"/>
+        </Head>
         <body>
           <Main />
           <script src="/prism.js" />
