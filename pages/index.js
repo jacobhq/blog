2843abf2299d2e5dcbf7e52e1d04bb348f5c3aa0
Jacob Marshall <jacob.marshall.jem@gmail.com>
2021-04-04 08:26:07 +0100

    ✨ Projects section

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import Icon from '@mdi/react'
+import { mdiOpenInNew } from '@mdi/js'
 
 export default function Home({ allPostsData }) {
   return (
@@ -17,6 +19,29 @@ export default function Home({ allPostsData }) {
           <a href="https://twitter.com/yes_jacob_">Twitter</a>, and code with me on <a href="https://github.com/jacobhq">GitHub</a>.{' '}
           I’m coffee powered, so why not <a href="https://buymeacoffee.com/jem">buy me a coffee</a>.
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Projects</h2>
+        <ul className={utilStyles.list}>
+        <li className={utilStyles.listItem}>
+            <a href="https://desica.uk/" target="_blank">
+              Desica <Icon path={mdiOpenInNew} size={1 + 'rem'} className={utilStyles.icon} />
+            </a>
+            <br />
+            <small className={utilStyles.lightText}>
+              Ongoing
+            </small>
+          </li>
+          <li className={utilStyles.listItem}>
+            <a href="https://mac.now.sh/" target="_blank">
+              MacOS Website Clone <Icon path={mdiOpenInNew} size={1 + 'rem'} className={utilStyles.icon} />
+            </a>
+            <br />
+            <small className={utilStyles.lightText}>
+              2021
+            </small>
+          </li>
+        </ul>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
